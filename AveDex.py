d79511dc329568972d49e67306bbba027f63971b
Walquiria Mafado <walquiriaifmg@gmail.com>
2026-06-23 20:29:13 -0300

    Registra versão com catálogo de aves e funções.

---
+++ AveDex.py
@@ -0,0 +1,18 @@
+catalogo_aves = [
+    {
+        "id": 1,
+        "nome_popular": "Bem-te-vi",
+        "nome_cientifico": "Pitangus sulphuratus",
+        "habitat": "Áreas abertas, cidades e bordas de florestas",
+        "alimentacao": "Insetos, frutos e pequenos animais",
+        "curiosidade": "Seu canto parece dizer o próprio nome."
+    },
+    {
+        "id": 2,
+        "nome_popular": "João-de-barro",
+        "nome_cientifico": "Furnarius rufus",
+        "habitat": "Campos, cidades e áreas rurais",
+        "alimentacao": "Insetos e outros invertebrados",
+        "curiosidade": "É conhecido por construir ninhos de barro."
+    }
+]
