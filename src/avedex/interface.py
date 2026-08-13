26260de4f868f4da7f400a5ca023be019e123a04
Walquiria Mafado <walquiriaifmag@gmail.com>
2026-08-13 17:52:39 -0300

    Cria modulo de interface da AveDex

---
+++ src/avedex/interface.py
@@ -0,0 +1,25 @@
+from src.avedex.utils import titulo, linha
+
+OPCOES_MENU = [
+    "1 - Listar aves",
+    "2 - Buscar ave",
+    "3 - Ver detalhes de uma ave",
+    "4 - Comparar duas aves",
+    "5 - Sobre a AveDex",
+    "0 - Sair"
+]
+
+def abertura(aves):
+    # Mostra uma abertura simples do sistema.
+    titulo("BEM-VINDO AO AVEDEX")
+    
+    print("Catálogo interativo de aves.")
+    print(f"Total de aves carregadas: {len(aves)}")
+    print(linha("-"))
+
+def exibir_menu():
+    # Exibe o menu principal com base na lista OPCOES_MENU.
+    titulo("AVEDEX - MENU PRINCIPAL")
+
+    for opcao in OPCOES_MENU:
+        print(opcao)
